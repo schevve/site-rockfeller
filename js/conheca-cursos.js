c96183966a726c5f2333f988a79a6b62514ed6fd
Samuel <147870470+schevve@users.noreply.github.com>
2025-06-14 18:04:15 -0300

    seção de materiais agora scrolla até o material, em vez de só trocar

--- js/conheca-cursos.js
+++ js/conheca-cursos.js
@@ -1,10 +1,14 @@
 function switchConhecaCursos(elementoMostrado){
+    let container = document.querySelector(".conheca-conteudo-animation-container");
     let cursos = document.querySelectorAll(".conheca-conteudo");
-    for (curso of cursos){
+    for (curso of cursos) {
         if (curso.id == elementoMostrado){
-            curso.style.display = "flex";
-        }else{
-            curso.style.display = "none";
+            let containerRect = container.getBoundingClientRect();
+            let elementRect = curso.getBoundingClientRect();
+            //let leftPercentage = containerRect.left
+            container.style.left = ((((elementRect.left - containerRect.left) / containerRect.width) * -100) + "%");
+            console.log(((((elementRect.left - containerRect.left) / containerRect.width) * -100) + "%"));
+            console.log(elementRect.left);
         }
     }
 }
